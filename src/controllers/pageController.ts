bbaa211f1aa95874b89d1cffe7b7da4bbdfc9eb0
GitCacaTyu <carlosalbertolacerdadias@gmail.com>
2025-09-16 17:50:30 -0300

    Base criada

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+
+export const home = (req: Request, res: Response) => {
+    res.send('Página inicial do Canil!');
+};
+
+
+export const dogs = (req: Request, res: Response) => {
+    res.send('Página inicial do Canil!');
+};
+
+
+export const cats = (req: Request, res: Response) => {
+    res.send('Página inicial do Canil!');
+};
+
+
+export const fishes = (req: Request, res: Response) => {
+    res.send('Página inicial do Canil!');
+};
